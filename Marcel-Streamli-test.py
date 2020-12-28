5ac5523bd7ade8f8d99de7c553059480163011c5
marcelhormozian <65503322+marcelhormozian@users.noreply.github.com>
2020-12-28 18:57:48 +0100

    Update Marcel-Streamli-test.py

--- Marcel-Streamli-test.py
+++ Marcel-Streamli-test.py
@@ -30,7 +30,7 @@ with st.echo(code_location='below'):
         y = radius * math.sin(angle)
         data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=200, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
                 
